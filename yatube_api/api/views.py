d63b7a66e910e47785a8778b3b4aef897b7a9931
Александр Денисов <akatsuki_sanya@vk.com>
2024-05-21 10:47:24 +0500

    V1.1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 
 from rest_framework.response import Response
 
